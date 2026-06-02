0949b65ccae3c0e567019411f071b6833c915211
Nived Mohan <161240240+nivedmohan77@users.noreply.github.com>
2026-06-02 23:04:16 +0530

    Update update-manifest-image.sh

--- ops/scripts/update-manifest-image.sh
+++ ops/scripts/update-manifest-image.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Print commands and their arguments as they are executed
 set -x
+# Ensure pipeline failures are caught
+set -o pipefail
 
 # Set the repository URL
 REPO_URL="https://<ACCESS-TOKEN>@dev.azure.com/<AZURE-DEVOPS-ORG-NAME>/voting-app/_git/voting-app"
@@ -8,8 +13,8 @@ REPO_URL="https://<ACCESS-TOKEN>@dev.azure.com/<AZURE-DEVOPS-ORG-NAME>/voting-ap
 # Clone the git repository into the /tmp directory
 git clone "$REPO_URL" /tmp/temp_repo
 
-# Navigate into the cloned repository directory
-cd /tmp/temp_repo
+# Navigate into the cloned repository directory (and fail safely if it doesn't exist)
+cd /tmp/temp_repo || exit 1
 
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
